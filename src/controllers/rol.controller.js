027f1bf32da28df30a070031d4bacfb2ce1d8f5e
rabi <rabi_leon@hotmail.com>
2020-09-08 22:55:26 -0500

    :recycle: se agrega try catct en usuarios y roles

--- src/controllers/rol.controller.js
+++ src/controllers/rol.controller.js
@@ -1,80 +1,109 @@
 import Rol from '../models/Roles';
 
 export async function getRoles(req, res) {
-    const roles = await Rol.findAll();
-    res.json({
-        data: roles
-    })
+    try {
+        const roles = await Rol.findAll();
+        res.json({
+            data: roles
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: {
+                code: "ERROR",
+                http_code: 500,
+                message: 'Somethin goes wrong' + error
+            }
+        })
+    }
 }
 
-export async function createRol(req,res) {
+export async function createRol(req, res) {
     const { name, description, slug, special } = req.body;
     try {
         let newRol = await Rol.create({
             name,
             description,
-            slug, 
+            slug,
             special
         },
-        {
-            fields: [ 'name','description','slug','special']
-        });
+            {
+                fields: ['name', 'description', 'slug', 'special']
+            });
         if (newRol) {
             return res.json({
                 message: 'Role created successfully',
                 data: newRol
-            });        
+            });
         }
     } catch (error) {
         res.status(500).json({
-            message: 'Somethin goes wrong'+ error,
-            data:{}
-        })
+            error: {
+                code: "ERROR",
+                http_code: 500,
+                message: 'Somethin goes wrong' + error
+            }
+        });
     }
 }
 
 export async function deleteRol(req, res) {
-    const { id } = req.params;
-
-    const deleteRowCount = await Rol.destroy({
-        where: {
-            id
-        }
-    });
-    res.json({
-        message: 'Rol deleted',
-        count: deleteRowCount
-    })
+    try {
+        const { id } = req.params;
+        const deleteRowCount = await Rol.destroy({
+            where: {
+                id
+            }
+        });
+        res.json({
+            message: 'Rol deleted',
+            count: deleteRowCount
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: {
+                code: "ERROR",
+                http_code: 500,
+                message: 'Somethin goes wrong' + error
+            }
+        });
+    }
 }
 
 
 export async function updateRol(req, res) {
-    const { id } = req.params;
-    const { name, description, slug, special } = req.body;
+    try {
+        const { id } = req.params;
+        const { name, description, slug, special } = req.body;
 
-    const data = await Rol.findAll({
-        attributes: ['id','name', 'description', 'slug', 'special'],
-        where: {
-            id
+        const data = await Rol.findAll({
+            attributes: ['id', 'name', 'description', 'slug', 'special'],
+            where: {
+                id
+            }
+        });
+        if (data.length > 0) {
+            data.forEach(async Rol => {
+                await Rol.update({
+                    name,
+                    description,
+                    slug,
+                    special
+                });
+            })
         }
-    });
-    if ( data.length > 0 ) {
-        data.forEach(async Rol =>{
-            await Rol.update({
-                name,
-                description,
-                slug,
-                special
-            });
-        } )
+
+
+        return res.json({
+            message: 'Rol Updated Succefully',
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: {
+                code: "ERROR",
+                http_code: 500,
+                message: 'Somethin goes wrong' + error
+            }
+        });
     }
-
-
-    return res.json({
-        message: 'Rol Updated Succefully',
-        data: data
-    })
-
-
-
 }
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,95 +1,127 @@
 import User from '../models/User';
 
 export async function getUsers(req, res) {
-    const users = await User.findAll();
-    res.json({
-        data: users
-    })
+    try {
+        const users = await User.findAll();
+        res.json({
+            data: users
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: {
+                code: "ERROR",
+                http_code: 500,
+                message: 'Somethin goes wrong' + error
+            }
+        })
+    }
 }
 
-export async function createUser(req,res) {
+export async function createUser(req, res) {
     const { roleid, parentuserid, groupid, name, lastname, email, phone, password, remembertoken, age, avatar } = req.body;
     try {
         let newUser = await User.create({
-            roleid, 
-            parentuserid, 
-            groupid, 
-            name, 
-            lastname, 
-            email, 
-            phone, 
-            password, 
-            remembertoken, 
-            age, 
+            roleid,
+            parentuserid,
+            groupid,
+            name,
+            lastname,
+            email,
+            phone,
+            password,
+            remembertoken,
+            age,
             avatar
         },
-        {
-            fields: ['roleid', 'parentuserid', 'groupid', 'name', 'lastname', 'email', 'phone', 'password', 'remembertoken', 'age', 'avatar']
-        });
+            {
+                fields: ['roleid', 'parentuserid', 'groupid', 'name', 'lastname', 'email', 'phone', 'password', 'remembertoken', 'age', 'avatar']
+            });
         if (newUser) {
             return res.json({
                 message: 'User created successfully',
                 data: newUser
-            });        
+            });
         }
     } catch (error) {
         res.status(500).json({
-            message: 'Somethin goes wrong'+ error,
-            data:{}
-        })
+            error: {
+                code: "ERROR",
+                http_code: 500,
+                message: 'Somethin goes wrong' + error
+            }
+        });
     }
 
 }
 
 export async function deleteUser(req, res) {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const deleteRowCount = await User.destroy({
-        where: {
-            id
-        }
-    });
-    res.json({
-        message: 'User deleted',
-        count: deleteRowCount
-    })
+        const deleteRowCount = await User.destroy({
+            where: {
+                id
+            }
+        });
+        res.json({
+            message: 'User deleted',
+            count: deleteRowCount
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: {
+                code: "ERROR",
+                http_code: 500,
+                message: 'Somethin goes wrong' + error
+            }
+        });
+    }
 }
 
 
 export async function updateUser(req, res) {
-    const { id } = req.params;
-    const { roleid, parentuserid , groupid, name, lastname, email, phone, password, remembertoken, age, avatar } = req.body;
+    try {
+        const { id } = req.params;
+        const { roleid, parentuserid, groupid, name, lastname, email, phone, password, remembertoken, age, avatar } = req.body;
 
-    const data = await User.findAll({
-        attributes: ['id','roleid', 'parentuserid', 'groupid', 'name', 'lastname', 'email', 'phone', 'password', 'remembertoken', 'age', 'avatar'],
-        where: {
-            id
+        const data = await User.findAll({
+            attributes: ['id', 'roleid', 'parentuserid', 'groupid', 'name', 'lastname', 'email', 'phone', 'password', 'remembertoken', 'age', 'avatar'],
+            where: {
+                id
+            }
+        });
+        if (data.length > 0) {
+            data.forEach(async User => {
+                await User.update({
+                    roleid,
+                    parentuserid,
+                    groupid,
+                    name,
+                    lastname,
+                    email,
+                    phone,
+                    password,
+                    remembertoken,
+                    age,
+                    avatar
+                });
+            })
         }
-    });
-    if ( data.length > 0 ) {
-        data.forEach(async User =>{
-            await User.update({
-                roleid, 
-                parentuserid , 
-                groupid, 
-                name, 
-                lastname, 
-                email, 
-                phone, 
-                password, 
-                remembertoken,
-                age, 
-                avatar
-            });
-        } )
+
+
+        return res.json({
+            message: 'User Updated Succefully',
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: {
+                code: "ERROR",
+                http_code: 500,
+                message: 'Somethin goes wrong' + error
+            }
+        });
     }
-
-
-    return res.json({
-        message: 'User Updated Succefully',
-        data: data
-    })
-
 
 
 }
