b65b9b127f4a75aee4d2553d600e9b1bf4876f0a
Daniel Guecha <guechadaniel@gmail.com>
2020-09-08 22:47:05 -0500

    :sparkles: CRUD groups finished

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
-import { createGroup, getGroups } from '../controllers/group.controller'
+import { createGroup, getGroups, getOneGroup, deleteGroup, updateGroup} from '../controllers/group.controller'
 // /api/groups
 router.post('/', createGroup);
 router.get('/', getGroups);
+// /api/groups/:groupID
+router.get('/:id', getOneGroup);
+router.delete('/:id', deleteGroup);
+router.put('/:id', updateGroup);
 
 module.exports = router;
